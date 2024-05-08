af9d937864d855131ec33e21db1731bf5a683d95
Gchang7 <giancarlochang@hotmail.com>
2024-05-07 21:24:51 -0500

    ok

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# test
+# ProyectoWeb
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using CapaModelo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using VotacionWeb.Controllers;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var votacion = new VotacionController();
+            var votante = new Votante()
+            {
+                DocumentoIdentidad = string.Empty
+            };
+            var rpta = votacion.Ingreso(votante.DocumentoIdentidad);
+            Assert.AreEqual(65765888, rpta);
+        }
+    }
+}
